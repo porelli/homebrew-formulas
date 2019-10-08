f9fccde7afca2ba49c0e9f3f52b60c97c8635559
Junfeng Li <lijunfen@amazon.com>
2019-10-08 14:29:29 -0700

    Binary release

--- Formula/unison-fsmonitor.rb
+++ Formula/unison-fsmonitor.rb
@@ -1,16 +1,16 @@
 class UnisonFsmonitor < Formula
   desc "unison-fsmonitor for macOS"
   homepage "https://github.com/autozimu/unison-fsmonitor"
-  url "https://github.com/autozimu/unison-fsmonitor/archive/0.2.3.tar.gz"
-  sha256 "c06a77b018bf8bb0127032b83fcb02af592f7002a0cbe812fb0e2655f50df08c"
+  url "https://github.com/autozimu/unison-fsmonitor/releases/latest/download/unison-fsmonitor.tar.gz"
+  sha256 "18bfdb1dea2fb66413e3240798d6eeba40aa8c4ca7664c252e7f78bdd894d937"
+  version "0.2.3"
   head "https://github.com/autozimu/unison-fsmonitor.git"
 
-  depends_on "rust" => :build
-
   def install
-    system "cargo", "install", "--root", prefix, "--path", "."
+    bin.install "unison-fsmonitor"
   end
 
   test do
   end
+
 end
