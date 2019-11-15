b64bfbec5005d58f3c4c9c9684a7429e4f13922e
Junfeng Li <lijunfen@amazon.com>
2019-11-14 22:25:46 -0800

    Remove sha256 and version from formula

--- Formula/unison-fsmonitor.rb
+++ Formula/unison-fsmonitor.rb
@@ -2,8 +2,6 @@ class UnisonFsmonitor < Formula
   desc "unison-fsmonitor for macOS"
   homepage "https://github.com/autozimu/unison-fsmonitor"
   url "https://github.com/autozimu/unison-fsmonitor/releases/latest/download/unison-fsmonitor.tar.gz"
-  sha256 "18bfdb1dea2fb66413e3240798d6eeba40aa8c4ca7664c252e7f78bdd894d937"
-  version "0.2.3"
   head "https://github.com/autozimu/unison-fsmonitor.git"
 
   def install
