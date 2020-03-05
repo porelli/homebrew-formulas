2f4a0974d5fb5ff2eb690a09c371d1828e094e95
Junfeng Li <lijunfen@amazon.com>
2020-03-05 10:09:38 -0800

    Fix version (nil) error when install

--- Formula/unison-fsmonitor.rb
+++ Formula/unison-fsmonitor.rb
@@ -1,8 +1,10 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
 class UnisonFsmonitor < Formula
   desc "unison-fsmonitor for macOS"
   homepage "https://github.com/autozimu/unison-fsmonitor"
   url "https://github.com/autozimu/unison-fsmonitor/releases/latest/download/unison-fsmonitor.tar.gz"
-  head "https://github.com/autozimu/unison-fsmonitor.git"
+  version "0.2.3"
 
   def install
     bin.install "unison-fsmonitor"
