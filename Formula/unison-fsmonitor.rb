0bdfcf709b86e843bd738179b57a97f0d18e6d1c
Michele Porelli <Linux571@gmail.com>
2022-11-15 09:50:12 +1100

    Bump to v0.3.2

--- Formula/unison-fsmonitor.rb
+++ Formula/unison-fsmonitor.rb
@@ -7,17 +7,17 @@ class UnisonFsmonitor < Formula
 
   if OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/autozimu/unison-fsmonitor/releases/download/v0.3.0/unison-fsmonitor-linux-amd64.tar.gz"
+      url "https://github.com/porelli/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-linux-amd64.tar.gz"
     elsif Hardware::CPU.arm?
-      url "https://github.com/autozimu/unison-fsmonitor/releases/download/v0.3.0/unison-fsmonitor-linux-aarch64.tar.gz"
+      url "https://github.com/porelli/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-linux-aarch64.tar.gz"
     else
       raise "Unsupported Linux arch!"
     end
   elsif OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/autozimu/unison-fsmonitor/releases/download/v0.3.0/unison-fsmonitor-macos-amd64.tar.gz"
+      url "https://github.com/porelli/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-amd64.tar.gz"
     elsif Hardware::CPU.arm?
-      url "https://github.com/autozimu/unison-fsmonitor/releases/download/v0.3.0/unison-fsmonitor-macos-aarch64.tar.gz"
+      url "https://github.com/porelli/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-aarch64.tar.gz"
     else
       raise "Unsupported macOS arch!"
     end
