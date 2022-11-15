5f54834b86aa0dcff3bd1af71d22ada80c085550
Michele Porelli <linux571@gmail.com>
2022-11-14 23:13:01 -0500

    restore autozimu as upstream source

--- Formula/unison-fsmonitor.rb
+++ Formula/unison-fsmonitor.rb
@@ -6,18 +6,12 @@ class UnisonFsmonitor < Formula
   version "0.3.2"
 
   if OS.linux?
-    if Hardware::CPU.intel?
-      url "https://github.com/porelli/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-linux-amd64.tar.gz"
-    elsif Hardware::CPU.arm?
-      url "https://github.com/porelli/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-linux-aarch64.tar.gz"
-    else
-      raise "Unsupported Linux arch!"
-    end
+    raise "formula for unison already provides unison-fsmonitor!"
   elsif OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/porelli/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-amd64.tar.gz"
+      url "https://github.com/autozimu/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-amd64.tar.gz"
     elsif Hardware::CPU.arm?
-      url "https://github.com/porelli/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-aarch64.tar.gz"
+      url "https://github.com/autozimu/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-aarch64.tar.gz"
     else
       raise "Unsupported macOS arch!"
     end
