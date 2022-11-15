705e88c721a430463e30228bcb7d268a8fcc53f8
Michele Porelli <linux571@gmail.com>
2022-11-14 23:13:30 -0500

    add support for macOS amd64 and aarch64, exclude Linux and other OSs

--- Formula/unison-fsmonitor.rb
+++ Formula/unison-fsmonitor.rb
@@ -3,9 +3,22 @@
 class UnisonFsmonitor < Formula
   desc "unison-fsmonitor for macOS"
   homepage "https://github.com/autozimu/unison-fsmonitor"
-  url "https://github.com/autozimu/unison-fsmonitor/releases/download/v0.3.0/unison-fsmonitor.tar.gz"
-  version "0.3.0"
+  version "0.3.2"
 
+  if OS.linux?
+    raise "formula for unison already provides unison-fsmonitor!"
+  elsif OS.mac?
+    if Hardware::CPU.intel?
+      url "https://github.com/autozimu/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-amd64.tar.gz"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/autozimu/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-aarch64.tar.gz"
+    else
+      raise "Unsupported macOS arch!"
+    end
+  else
+    raise "Unsupported OS!"
+  end
+    
   def install
     bin.install "unison-fsmonitor"
   end
