a2ac8b840258ca6b55f4daddd4d3a27fc47e845e
Michele Porelli <Linux571@gmail.com>
2022-11-14 23:34:02 -0500

    repo change to allow macos aarch64 binary testing while waiting for PR

--- Formula/unison-fsmonitor.rb
+++ Formula/unison-fsmonitor.rb
@@ -9,9 +9,9 @@ class UnisonFsmonitor < Formula
     raise "formula for unison already provides unison-fsmonitor!"
   elsif OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/autozimu/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-amd64.tar.gz"
+      url "https://github.com/porelli/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-amd64.tar.gz"
     elsif Hardware::CPU.arm?
-      url "https://github.com/autozimu/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-aarch64.tar.gz"
+      url "https://github.com/porelli/unison-fsmonitor/releases/download/v0.3.2/unison-fsmonitor-macos-aarch64.tar.gz"
     else
       raise "Unsupported macOS arch!"
     end
